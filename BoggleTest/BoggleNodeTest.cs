a570d714e4bd3ac3cdc82d40a5d4cfb395cccccf
Amir Kouretchian <amir.kouretchian@gmail.com>
2016-02-06 09:47:42 -0800

    finished tests for node IsImmediatelyBelow

--- BoggleTest/BoggleNodeTest.cs
+++ BoggleTest/BoggleNodeTest.cs
@@ -65,13 +65,57 @@ namespace BoggleTest
         [TestMethod]
         public void BoggleNode_IsImmediatelyBelow_ReturnsTrue_WhenCallerIsImmediatelyBelowArg()
         {
-            // todo
+            BoggleNode n1 = new BoggleNode();
+            BoggleNode n2 = new BoggleNode();
+
+            n1.X = n1.Y = 10;
+            n2.Y = n1.Y - 1;
+            n2.X = n1.X;
+            Assert.IsTrue(n1.IsImmediatelyBelow(n2));
         }
 
         [TestMethod]
         public void BoggleNode_IsImmediatelyBelow_ReturnsFalse_WhenCallerIsNotImmediatelyBelowArg()
         {
-            // todo
+            BoggleNode n1 = new BoggleNode();
+            BoggleNode n2 = new BoggleNode();
+
+            n1.X = n1.Y = 10;
+
+            // top-left
+            n2.X = n1.X + 1;
+            n2.Y = n1.Y + 1;
+            Assert.IsFalse(n1.IsImmediatelyBelow(n2));
+
+            // top
+            n2.X = n1.X;
+            n2.Y = n1.Y + 1;
+            Assert.IsFalse(n1.IsImmediatelyBelow(n2));
+
+            // top-right
+            n2.X = n1.X - 1;
+            n2.Y = n1.Y - 1;
+            Assert.IsFalse(n1.IsImmediatelyBelow(n2));
+
+            // right
+            n2.X = n1.X - 1;
+            n2.Y = n1.Y;
+            Assert.IsFalse(n1.IsImmediatelyBelow(n2));
+
+            // bottom-right
+            n2.X = n1.X - 1;
+            n2.Y = n1.Y - 1;
+            Assert.IsFalse(n1.IsImmediatelyBelow(n2));
+
+            // bottom-left
+            n2.X = n1.X + 1;
+            n1.Y = n1.Y - 1;
+            Assert.IsFalse(n1.IsImmediatelyBelow(n2));
+
+            // left
+            n2.X = n1.X + 1;
+            n2.Y = n1.Y;
+            Assert.IsFalse(n1.IsImmediatelyBelow(n2));
         }
     }
 }
